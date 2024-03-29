d4593edbc9ed0944b624818713b2db4165bf8a20
Kevin Csellak <kevincsellak@gmail.com>
2024-03-29 20:03:25 +0100

    implemented visualization properly

--- program_parts/visualization_stuff.r
+++ program_parts/visualization_stuff.r
@@ -2,52 +2,62 @@ source("./program_parts/global_functions_stuff.r")
 
 visualize_data <- function(configuration) {
     # TODO: make this at least somewhat functioning.
-    return(configuration)
     library(ggplot2)
 
-    collected_data <- display_data_bank$sims$`sim 1`$collected_data
+    # make sure there's a player at the end
+    contestants <- configuration$contestants
+    if ((length(contestants) == 0) || (contestants[[length(contestants)]]$playstyle != "player")) {
+        return(configuration)
+    }
 
-    the_y_axis <- c()
-    for (round_name in names_or_seq_along(collected_data)) {
-        round_data <- collected_data[[round_name]]
+    if (!hasName(configuration$persistent_stuff, "plottables")) {
+        configuration$persistent_stuff$plottables <- list()
+    }
 
+    # we get the data we'll visualize and we get the persistent data we kept about the previous suites
+    sim_data <- configuration$data_store$simulations[["simulation 1"]]
+    plottables <- configuration$persistent_stuff$plottables
+
+    # gather the data we want to visualize from this round
+    plottable <- c()
+    for (round_name in names_or_seq_along(sim_data)) {
+        round_data <- sim_data[[round_name]]
         reality <- round_data$reality
-        the_player_index <- length(display_data_bank$suite$compiled_contestants)
-
-        # The first of these two is for if you save all contestant guesses,
-        # The second of the two is for if you only have the player guess
-        # the_player_prob_for_reality <- round_data$contestant_guesses[[the_player_index]][[reality]]
-        the_player_prob_for_reality <- round_data$contestant_guesses[[1]][[reality]]
-
-        the_y_axis <- c(the_y_axis, the_player_prob_for_reality)
+        player_index <- length(contestants)
+        player_probability <- round_data$guesses[[player_index]][[reality]]
+        plottable <- c(plottable, player_probability)
     }
 
-    if (!exists("the_y_axes", inherits = TRUE)) {
-        the_y_axes <<- list()
-    }
-
-    contestants <- display_data_bank$suite$compiled_contestants
-    number_of_normals <- length(contestants[contestants == "normalm"])
+    # get the number of each important contestant type here
+    number_of_cheaters <- length(contestants[contestants == "cheater"])
     number_of_wrongs <- length(contestants[contestants == "wrong"])
     number_of_uniforms <- length(contestants[contestants == "uniform"])
 
-    the_series_label <- paste0("Ω|=", as.character(display_data_bank$suite$omega_size))
-    the_series_label <- paste0(the_series_label, ",\n#No=", as.character(number_of_normals))
-    the_series_label <- paste0(the_series_label, ",\n#Wr=", as.character(number_of_wrongs))
-    the_series_label <- paste0(the_series_label, ",\n#Un=", as.character(number_of_uniforms))
+    # create a label for this plot line
+    the_series_label <- paste0("Ω=", as.character(configuration$omega_size))
+    the_series_label <- paste0(the_series_label, ", #Ch=", as.character(number_of_cheaters))
+    the_series_label <- paste0(the_series_label, ", #Wr=", as.character(number_of_wrongs))
+    the_series_label <- paste0(the_series_label, ", #Un=", as.character(number_of_uniforms))
 
-    the_y_axes[[the_series_label]] <- the_y_axis
-    # print(the_y_axes)
-    the_series <- names(the_y_axes)
-    y_dataframe <- as.data.frame(the_y_axes)
-    # names(y_dataframe) <- sub(",", ",\n ", names(y_dataframe))
-    y_dataframe$rounds <- 1:display_data_bank$suite$rounds_per_simulation
-    df <- reshape2::melt(y_dataframe, id.vars = "rounds", variable.name = "series")
+    # add the data to the plottables
+    plottables[[the_series_label]] <- plottable
+
+    # save the new plottables in the persistent stuff
+    configuration$persistent_stuff$plottables <- plottables
+
+    # convert it to an actually plottable format
+    the_series <- names(plottables)
+    plottables_dataframe <- as.data.frame(plottables)
+    plottables_dataframe$rounds <- 1:configuration$rounds_per_simulation
+    df <- reshape2::melt(plottables_dataframe, id.vars = "rounds", variable.name = "series")
+
+    # plot it
     pictureplot <- ggplot(df, aes(rounds, value)) +
         geom_line(aes(colour = series))
-    # pictureplot <- pictureplot + scale_colour_manual(breaks=unlist(the_series), values = colors())
-    # pictureplot <- pictureplot + theme(legend.position="bottom")
-    # pictureplot <- pictureplot + scale_colour_discrete(labels = function(x) stringr::str_wrap(x, width = 5))
+
+    # print it
     print(pictureplot)
-    the_y_axes <<- the_y_axes
+
+    # return the configuration
+    return(configuration)
 }
